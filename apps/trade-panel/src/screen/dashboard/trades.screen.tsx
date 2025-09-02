4a555c7a5fddaf8fcb58f4019db50f0ab5ec949f
Ríder Cantuária <dev.cantuaria@gmail.com>
2025-09-02 14:50:24 -0300

    feat(panel): fix application header and add portfolio summary

--- apps/trade-panel/src/screen/dashboard/trades.screen.tsx
+++ apps/trade-panel/src/screen/dashboard/trades.screen.tsx
@@ -1,9 +1,12 @@
+import { PortfolioSummary } from "@/src/components/portfolio/portfolio-summary.component";
 import React from "react";
 
 export function TradesScreen() {
 	return (
 		<div className="flex mb-[1rem] p-[1rem] h-fit gap-4 mt-[3.8125rem]">
-			<div></div>
+			<div>
+				<PortfolioSummary />
+			</div>
 
 			<div className="w-[450px]"></div>
 		</div>
