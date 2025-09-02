3df1d4c094b4c0ca6c5f010b2a35a1b5e5e1f54b
Ríder Cantuária <dev.cantuaria@gmail.com>
2025-09-02 11:19:35 -0300

    chore(api): add validation dto pipe

---
+++ apps/trade-api/src/pipes/dto-validation.pipe.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
+
+export class DTOValidationPipe extends ValidationPipe {
+  constructor() {
+    super({
+      whitelist: true, // Strip properties that do not have decorators
+      forbidNonWhitelisted: true, // Throw error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to DTO instances
+      exceptionFactory: (errors) => {
+        console.log("Failed to validate inputs: ", errors);
+
+        // Get all input errors from the request, to easily manage the input errors on API response
+        const inputs = errors.reduce(
+          (acc, err) => {
+            acc[err.property] = Object.values(err.constraints || {});
+            return acc;
+          },
+          {} as Record<string, string[]>,
+        );
+
+        return new BadRequestException({
+          message: "Bad request",
+          inputs,
+        });
+      },
+    });
+  }
+}
