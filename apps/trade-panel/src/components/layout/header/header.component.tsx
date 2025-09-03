908571cf28601144c3945d8b4ba5d117a2983617
Ríder Cantuária <dev.cantuaria@gmail.com>
2025-09-03 03:17:22 -0300

    feat(panel): remove unused menu on header

--- apps/trade-panel/src/components/layout/header/header.component.tsx
+++ apps/trade-panel/src/components/layout/header/header.component.tsx
@@ -34,19 +34,6 @@ export function Header() {
 					<span className="flex items-center text-foreground-secondary">LOGO</span>
 
 					<div className="bg-[#4C4D52] w-[1px] h-[50px] mx-[1.5rem]"></div>
-
-					<ul className="flex gap-4 h-full">
-						<li className="relative flex justify-center items-center h-full px-[1rem] after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-blue-500">
-							<a href="#" className="text-blue-500">
-								Portfolios
-							</a>
-						</li>
-						<li className="flex justify-center items-center h-full px-[1rem]">
-							<a href="#" className="text-foreground-secondary">
-								Docs
-							</a>
-						</li>
-					</ul>
 				</nav>
 
 				<div className="flex items-center">
