e46b9082daf419a3f6983b4ee746da99da96f95d
Ríder Cantuária <dev.cantuaria@gmail.com>
2025-09-03 18:11:27 -0300

    chore: fix Docker to orchestrate multiple services

--- apps/trade-api/src/main.ts
+++ apps/trade-api/src/main.ts
@@ -4,7 +4,6 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 import { PrismaExceptionFilter } from "./filters/prisma-exception.filters";
-import { BadRequestException, ValidationPipe } from "@nestjs/common";
 import { DTOValidationPipe } from "./pipes/dto-validation.pipe";
 import { AllExceptionsFilter } from "./filters/all-exception.filters";
 
